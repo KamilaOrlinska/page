3d1c4768a35886fa27c2a3a255cadcca86c24fd6
KamilaOrlinska <kamila.orlinska@icloud.com>
2018-09-19 14:47:36 +0200

    [E-01130] text change

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const App = () =>{
             <Header/>
         </div>
     )
-    //return React.createElement('h1',{className:'title'},'Hello world!!!',React.createElement('div'))
+    //return React.createElement('h1',{className:'title'},'Hello world!!! How are you guys?',React.createElement('div'))
 }
 
 
