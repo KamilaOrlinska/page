f9d503db7d5ccf7e2b9d4623d36d40cece9989b3
KamilaOrlinska <kamila.orlinska@icloud.com>
2018-09-19 14:51:42 +0200

    [E-01130] text change

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const App = () =>{
             <Header/>
         </div>
     )
-    //return React.createElement('h1',{className:'title'},'Hello world!!! How are you guys?',React.createElement('div'))
+    //return React.createElement('h1',{className:'title'},'Hello world!!! How are you guys? making the webhook working, woud it work this time?',React.createElement('div'))
 }
 
 
